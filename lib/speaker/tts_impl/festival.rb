9469df712ffce1b2c79f00f60ab27fcbbc0ef0c4
airninja <airninja@vm-ubuntu.(none)>
2008-12-22 09:20:08 +0100

    Tweaked lame options for festival impl.

--- lib/speaker/tts_impl/festival.rb
+++ lib/speaker/tts_impl/festival.rb
@@ -4,7 +4,7 @@ module Speaker
       options = {:filename => "test.mp3"}
       options.merge!(opts)
       
-      if system(%{echo "#{sanitize(text.to_s)}" | text2wave | lame --alt-preset cbr 16 -a --resample 11 --lowpass 5 -X3 - > #{options[:filename]}})
+      if system(%{echo "#{sanitize(text.to_s)}" | text2wave | lame -V2 - > #{options[:filename]}})
         options[:filename]
       else
         raise "Error while running festival. You don't seem to have festival or lame installed ..." 
