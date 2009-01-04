1494088c4a2753636bd5d61aa30f3f02017f220e
Dejan Simic <desimic@gmail.com>
2009-01-04 04:27:00 +0100

    Initial code commit.

---
+++ examples/examples.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/../lib/espeak-ruby'
+
+mp3_file = Espeak::Mp3.create "Hello World", :filename => "hello_world.mp3", :pitch => 30, :speed => 100, :voice => :sr
+puts "Successfully created #{mp3_file}!"
