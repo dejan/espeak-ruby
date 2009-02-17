f42e1d8d101adeab9d844b16d942adfd3a4405ae
Dejan Simic <desimic@gmail.com>
2009-02-17 02:12:59 +0100

    Options support in speaker-http

--- lib/speaker-http.rb
+++ lib/speaker-http.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 require File.dirname(__FILE__) + '/speaker'
 
 get '/speaker' do
-  text = params[:text]
+  text = params.delete(:text)
   
-  [200, {'Content-type' => 'audio/mpeg'}, File.read(Speaker::Mp3.create(text))]
+  [200, {'Content-type' => 'audio/mpeg'}, File.read(Speaker::Mp3.create(text, params))]
 end
