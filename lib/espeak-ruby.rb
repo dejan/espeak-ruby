194c50d5899888be6739e2ba69c461fa56be29fb
Dejan Simic <desimic@gmail.com>
2009-02-17 23:45:36 +0100

    Loose the active_support dependency; Filename is hashed value of options.

--- lib/espeak-ruby.rb
+++ lib/espeak-ruby.rb
@@ -1,19 +1,37 @@
 require 'rubygems'
-require 'active_support'
+require 'digest/sha1'
 
 module ESpeak
+
   def espeak(text, opts = {})
-
-    options = {:pitch => 30, :speed => 100, :filename => "test.mp3", :lang => 'en'}
     opts.symbolize_keys!
-    options.merge!(opts)
+    options = {
+      :v => 'en',   # use voice file of this name from espeak-data/voices
+      :p => 50,     # pitch adjustment, 0 to 99
+      :s => 170     # speed in words per minute, 80 to 370
+    }.merge(opts)
+    
     sanitized_text = text.gsub(/(!|\?|"|`|\\)/, ' ')
+    filename = Digest::SHA1.hexdigest(options.to_s)
 
-    if system(%$espeak "#{sanitized_text}" --stdout -v#{options[:lang]} -p#{options[:pitch]} -s#{options[:speed]} | lame -V2 - #{options[:filename]}$)
-      options[:filename]
+    if system(%$espeak "#{sanitized_text}" --stdout -v#{options[:lang]} -p#{options[:pitch]} -s#{options[:speed]} | lame -V2 - #{filename}$)
+      filename
     else
       raise "Error while running espeak. You don't seem to have espeak or lame installed ..." 
     end
-
   end
+
+private
+
+  def symbolize_keys
+    inject({}) do |options, (key, value)|
+      options[(key.to_sym rescue key) || key] = value
+      options
+    end
+  end
+
+  def symbolize_keys!
+    self.replace(self.symbolize_keys)
+  end
+
 end
