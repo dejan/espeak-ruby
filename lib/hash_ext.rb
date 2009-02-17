7a6d1fea589c858c532dc59b37037d62bfc40ee7
Dejan Simic <desimic@gmail.com>
2009-02-18 00:26:11 +0100

    Added hash_ext.rb with symbolize_keys methods

---
+++ lib/hash_ext.rb
@@ -0,0 +1,13 @@
+# from ActiveSupport
+class Hash
+  def symbolize_keys
+    inject({}) do |options, (key, value)|
+      options[(key.to_sym rescue key) || key] = value
+      options
+    end
+  end
+
+  def symbolize_keys!
+    self.replace(self.symbolize_keys)
+  end
+end
