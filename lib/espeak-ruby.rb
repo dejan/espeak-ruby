459bf311159b20ee2a50fe1400121306f6c8198d
Dejan Simic <desimic@gmail.com>
2009-02-18 02:05:23 +0100

    Typo

--- lib/espeak-ruby.rb
+++ lib/espeak-ruby.rb
@@ -13,7 +13,7 @@ module ESpeak
     }.merge(opts)
     
     sanitized_text = text.gsub(/(!|\?|"|`|\\)/, ' ')
-    filename = Digest::SHA1.hexdigest(textrm + options.to_s + ".mp3")
+    filename = Digest::SHA1.hexdigest(text + options.to_s + ".mp3")
 
     if system(%$espeak "#{sanitized_text}" --stdout -v#{options[:v]} -p#{options[:p]} -s#{options[:s]} | lame -V2 - #{filename}$)
       filename
